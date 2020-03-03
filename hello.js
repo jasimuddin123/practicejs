9560b0ff5462e8443269d7433d66721dc16fef4c
jasimuddin123 <jasimuddindxn@gmail.com>
2020-03-03 13:14:18 -0800

    practice javascript condition

---
+++ hello.js
@@ -0,0 +1,20 @@
+
+var age =30;
+
+if(age>20){
+
+    console.log("Veyear boyos hoi nai");
+
+}else {
+    console.log("tumer voyos hoyaca");
+}
+
+var fishPrice = 10;
+
+if (fishPrice ==20){
+    console.log("Fish Price is equel ");
+}else{
+
+ 
+   console.log ("Fish price is not equal");
+}
